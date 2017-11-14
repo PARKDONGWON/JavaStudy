62b6d869997cc01d8d9bfeee285d25978f16d8a9
PARKDONGWON <qkrehddnjs05@naver.com>
2017-11-14 10:24:24 +0900

    ㄴㅁㅇㅁㄴㅇ

--- University/src/com/db/connection/DBconnection.java
+++ University/src/com/db/connection/DBconnection.java
@@ -14,7 +14,7 @@ public class DBconnection {
 				conn = DriverManager.getConnection(url,"root","1234");
 				System.out.println("DB 연결 성공");
 			}catch(Exception e) { // 오류를 잡아주는 최상위 클래스 
-				System.out.println("DB 연결 실패"+e);
+				System.out.println("DB 연결 실패"+e);ㅁㄴㅇㅁ
 			}
 		}
 		return conn;
